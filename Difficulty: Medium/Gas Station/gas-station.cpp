c732c5ec16d69f69c59346064cd4119f19172fbf
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2025-03-30 15:54:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        // Your code here
+         int totalGas = 0, totalCost = 0, startIndex = 0, tank = 0;
+        
+        for (int i = 0; i < gas.size(); i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+            
+            if (tank < 0) { // If we can't reach the next station
+                startIndex = i + 1; // Start from the next station
+                tank = 0; // Reset tank
+            }
+        }
+        
+        return (totalGas >= totalCost) ? startIndex : -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> gas, cost;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            gas.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            cost.push_back(number2);
+        }
+        Solution ob;
+        int ans = ob.startStation(gas, cost);
+
+        cout << ans << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
