b925f473eaead74bf00734454d7e74264fb9b104
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2025-04-18 18:03:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Trie/implement-trie.cpp
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+// class Trie {
+//   public:
+
+//     Trie() {
+//         // implement Trie
+//     }
+
+//     void insert(string &word) {
+//         // insert word into Trie
+//     }
+
+//     bool search(string &word) {
+//         // search word in the Trie
+//     }
+
+//     bool isPrefix(string &word) {
+//         // search prefix word in the Trie
+//     }
+// };
+
+class Trie {
+private:
+    struct TrieNode {
+        bool isEndOfWord;
+        TrieNode* children[26];
+
+        TrieNode() {
+            isEndOfWord = false;
+            for (int i = 0; i < 26; i++) {
+                children[i] = nullptr;
+            }
+        }
+    };
+
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool isPrefix(string &word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int q;
+        cin >> q;
+        cin.ignore();
+        vector<bool> ans;
+        Trie ob;
+        for (int i = 0; i < q; i++) {
+            int x;
+            cin >> x;
+            string s;
+            cin >> s;
+            cin.ignore();
+            if (x == 1) {
+                ob.insert(s);
+            } else if (x == 2) {
+                ans.push_back(ob.search(s));
+            } else if (x == 3) {
+                ans.push_back(ob.isPrefix(s));
+            }
+        }
+        cout << boolalpha;
+        for (auto it : ans) {
+            cout << it << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
