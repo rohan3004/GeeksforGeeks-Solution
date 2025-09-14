29c92769e0419780a2bb3d7e138b94f82c88ea6d
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2025-09-14 16:23:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,69 +1,46 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
+#include <numeric> // Required for std::accumulate in some compilers
 
-
-// } Driver Code Ends
-
-
-/*You are required to complete this method*/
 class Solution {
   public:
-    int startStation(vector<int> &gas, vector<int> &cost) {
-        // Your code here
-         int totalGas = 0, totalCost = 0, startIndex = 0, tank = 0;
+    /**
+     * @brief Finds the starting gas station index to complete a circular tour.
+     * @param gas A vector of integers representing gas at each station.
+     * @param cost A vector of integers representing the cost to travel to the next station.
+     * @return The index of the starting station, or -1 if no solution exists.
+     */
+    int startStation(std::vector<int> &gas, std::vector<int> &cost) {
+        int n = gas.size();
         
-        for (int i = 0; i < gas.size(); i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-            tank += gas[i] - cost[i];
+        // total_tank tracks the overall gas balance. If it's negative at the end,
+        // the tour is impossible.
+        int total_tank = 0;
+        
+        // current_tank tracks the gas for the current journey segment.
+        int current_tank = 0;
+        
+        // start_station holds the potential starting station index.
+        int start_station = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            int diff = gas[i] - cost[i];
             
-            if (tank < 0) { // If we can't reach the next station
-                startIndex = i + 1; // Start from the next station
-                tank = 0; // Reset tank
+            // Update the total and current tank levels.
+            total_tank += diff;
+            current_tank += diff;
+            
+            // If current_tank goes negative, it means we can't reach station i+1
+            // from the current start_station.
+            if (current_tank < 0) {
+                // The next station becomes the new candidate for the start.
+                start_station = i + 1;
+                // Reset the tank for this new journey segment.
+                current_tank = 0;
             }
         }
         
-        return (totalGas >= totalCost) ? startIndex : -1;
+        // If the total_tank is non-negative, a solution exists, and it's the 
+        // start_station we found. Otherwise, no solution exists.
+        return (total_tank >= 0) ? start_station : -1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
