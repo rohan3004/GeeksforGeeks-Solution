62468a35aeb001d0d72b4bf5431cba6fac489278
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2026-03-05 12:29:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.cpp
@@ -1,35 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
-public:
+  public:
     int longestKSubstr(string &s, int k) {
-        unordered_map<char,int> freq;
-        int distinct = 0;
-        int maxLen = -1;
-        int left = 0;
-        
-        for (int right = 0; right < (int)s.size(); ++right) {
-            char c = s[right];
-            if (freq[c] == 0) 
-                ++distinct;
-            ++freq[c];
-            
-            // If too many distinct, shrink from the left
-            while (distinct > k) {
-                char d = s[left++];
-                if (--freq[d] == 0) {
-                    freq.erase(d);
-                    --distinct;
+        int n = s.length();
+        int left = 0, maxLen = -1;
+        unordered_map<char, int> freq;
+
+        for (int right = 0; right < n; right++) {
+            // Add current character to the window
+            freq[s[right]]++;
+
+            // If we have more than k distinct characters, shrink from the left
+            while (freq.size() > k) {
+                freq[s[left]]--;
+                if (freq[s[left]] == 0) {
+                    freq.erase(s[left]);
                 }
+                left++;
             }
-            
-            // If exactly k distinct, update answer
-            if (distinct == k) {
+
+            // If we have exactly k characters, update maxLen
+            if (freq.size() == k) {
                 maxLen = max(maxLen, right - left + 1);
             }
         }
-        
+
         return maxLen;
     }
 };
