edf772da888670502919528fa720f93934a1c90e
Rohan Chakravarty <112606377+rohan3004@users.noreply.github.com>
2026-03-12 22:55:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
@@ -1,41 +1,39 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
- public:
-  int kBitFlips(vector<int>& arr, int k) {
-    int n = arr.size();
-    int flips = 0;
-    // This variable tracks the current flip state. 
-    // 0 means an even number of active flips cover this index.
-    // 1 means an odd number of active flips cover this index.
-    int current_flip_state = 0;
-
-    for (int i = 0; i < n; ++i) {
-      // If we are at least k elements in, a flip that started at `i-k`
-      // is now out of range and its effect should be removed.
-      if (i >= k) {
-        // We marked the start of a flip by adding 2.
-        // If arr[i-k] > 1, a flip was initiated there.
-        if (arr[i - k] > 1) {
-          current_flip_state ^= 1; // Toggle the state
+public:
+    int kBitFlips(vector<int>& arr, int k) {
+        int n = arr.size();
+        int currentFlips = 0;
+        int totalOperations = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            // If our sliding window moves past a previous flip's range, 
+            // we remove its effect by decrementing the flip counter.
+            if (i >= k && arr[i - k] == 2) {
+                currentFlips--;
+            }
+            
+            // Calculate the effective bit value at the current index.
+            // If it was 0 and flipped an even number of times, it's still 0.
+            // If it was 1 and flipped an odd number of times, it becomes 0.
+            if ((arr[i] + currentFlips) % 2 == 0) {
+                
+                // If we need to flip but there are fewer than 'k' elements left,
+                // it's impossible to fix the array.
+                if (i + k > n) {
+                    return -1;
+                }
+                
+                // Initiate a new flip starting at this index
+                totalOperations++;
+                currentFlips++;
+                arr[i] = 2; // Mark this index as the start of a flip
+            }
         }
-      }
-
-      // Check if the current element needs to be flipped.
-      // Its effective value is `arr[i] ^ current_flip_state`.
-      // If the effective value is 0, we must flip.
-      // This is equivalent to `arr[i] == current_flip_state`.
-      if (arr[i] == current_flip_state) {
-        // If a flip is needed but we don't have enough elements left
-        // for a full k-length subarray, it's impossible.
-        if (i + k > n) {
-          return -1;
-        }
-
-        // Perform the flip
-        flips++;
-        current_flip_state ^= 1; // The new flip affects the state
-        arr[i] += 2; // Mark that a flip started here
-      }
+        
+        return totalOperations;
     }
-    return flips;
-  }
 };
